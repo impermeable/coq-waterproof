afb7b6e0e58a886c1ca0ab76cd4468e2abaf3a7e
jim_jwp <j.w.portegies@tue.nl>
2023-07-10 15:32:36 +0200

    Remove many files

--- src/waterprove.mli
+++ src/waterprove.mli
@@ -17,55 +17,6 @@
 (******************************************************************************)
 
 (**
-  Is automation shield enabled ? 
-*)
-val automation_shield : bool ref
-
-(**
-  Waterprove
-
-  This function is the main automatic solver of coq-waterproof.
-
-  The databases used for the proof search are the one declared in the current imported dataset (see {! Hint_dataset.loaded_hint_dataset}).
-
-  The forbidden patterns are defined in {! is_forbidden}.
-
-  Arguments:
-    - [depth] ([int]): max depth of the proof search
-    - [shield] ([bool]): if set to [true], will stop the proof search if a forbidden pattern is found
-    - [lems] ([Tactypes.delayed_open_constr list]): additional lemmas that are given to solve the proof
-    - [database_type] ([Hint_dataset_declarations]): type of databases that will be use as hint databases
-*)
-val waterprove :
-  int ->
-  ?shield:bool ->
-  Tactypes.delayed_open_constr list ->
-  Hint_dataset_declarations.database_type ->
-  unit Proofview.tactic
-
-(**
-  Restricted Waterprove
-
-  This function is similar to {! waterprove} but use {! wp_auto.rwp_auto} and {! wp_eauto.rwp_eauto} instead of {! wp_auto.wp_auto} and {! wp_eauto.wp_eauto}.
-
-  Arguments:
-    - [depth] ([int]): max depth of the proof search
-    - [shield] ([bool]): if set to [true], will stop the proof search if a forbidden pattern is found
-    - [lems] ([Tactypes.delayed_open_constr list]): additional lemmas that are given to solve the proof
-    - [database_type] ([Hint_dataset_declarations]): type of databases that will be use as hint databases
-    - [must_use] ([string list]): list of hints that have to be used during the automatic solving
-    - [forbidden] ([string list]): list of hints that must not be used during the automatic solving
-*)
-val rwaterprove :
-  int ->
-  ?shield:bool ->
-  Tactypes.delayed_open_constr list ->
-  Hint_dataset_declarations.database_type ->
-  Evd.econstr list ->
-  Evd.econstr list ->
-  unit Proofview.tactic
-
-(**
   Sends a warning message and returns message as a string
 *)
 val print_warning :
--- theories/Waterprove.v
+++ theories/Waterprove.v
@@ -23,38 +23,4 @@ Require Import Waterproof.
 Require Import Ltac2.Bool.
 Require Import Ltac2.Init.
 
-Local Ltac2 Type database_type_ffi.
-
-Local Ltac2 @ external database_type_main: unit -> database_type_ffi := "coq-waterproof" "database_type_main".
-Local Ltac2 @ external database_type_decidability: unit -> database_type_ffi := "coq-waterproof" "database_type_decidability".
-Local Ltac2 @ external database_type_shorten: unit -> database_type_ffi := "coq-waterproof" "database_type_shorten".
-
-Local Ltac2 @ external waterprove_ffi: int -> bool -> (unit -> constr) list -> database_type_ffi -> unit := "coq-waterproof" "waterprove".
-Local Ltac2 @ external rwaterprove_ffi: int -> bool -> (unit -> constr) list -> database_type_ffi -> constr list -> constr list -> unit := "coq-waterproof" "rwaterprove".
-
 Ltac2 @ external print_warning_ffi : message -> unit := "coq-waterproof" "external_print_warning".
-
-Ltac2 Type database_type := [ Main | Decidability | Shorten ].
-
-Local Ltac2 database_type_to_ffi (db_type: database_type): database_type_ffi :=
-  match db_type with
-    | Main => database_type_main ()
-    | Decidability => database_type_decidability ()
-    | Shorten => database_type_shorten ()
-  end.
-
-Ltac2 contains_shilded_pattern (): bool :=
-  lazy_match! goal with
-    | [ |- forall _, _ ] => true
-    | [ |- exists _, _ ] => true
-    | [ |- _ /\ _] => true
-    | [ |- _ \/ _] => true
-    | [ |- _] => false
-  end
-.
-
-Ltac2 waterprove (depth: int) (shield: bool) (lems: (unit -> constr) list) (db_type: database_type): unit  :=
-  waterprove_ffi depth (shield && contains_shilded_pattern ()) lems (database_type_to_ffi db_type).
-
-Ltac2 rwaterprove (depth: int) (shield: bool) (lems: (unit -> constr) list) (db_type: database_type) (must_use: constr list) (forbidden: constr list): unit  :=
-  rwaterprove_ffi depth (shield && contains_shilded_pattern ()) lems (database_type_to_ffi db_type) must_use forbidden.
