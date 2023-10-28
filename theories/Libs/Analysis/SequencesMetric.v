bb1779f19a6fd5d9b740b5e102bd5b89fc731b0e
Jim Portegies <j.w.portegies@tue.nl>
2023-10-28 07:47:27 +0200

    feat: use N1 instead of N in definition convergence

--- theories/Libs/Analysis/SequencesMetric.v
+++ theories/Libs/Analysis/SequencesMetric.v
@@ -33,7 +33,7 @@ Definition convergence {M : Metric_Space}
                        (a : ℕ → Base M)
                        (c : Base M) :=
   ∀ ε : ℝ, ε > 0 ⇒
-    ∃ N : ℕ, ∀ n : ℕ, (n ≥ N)%nat ⇒
+    ∃ N1 : ℕ, ∀ n : ℕ, (n ≥ N1)%nat ⇒
       dist M (a n) c < ε.
 
 Definition bounded {X : Metric_Space} (a : ℕ → Base X) :=
