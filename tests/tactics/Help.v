f6404a4beb455d77f82d1160ec9bfed7b9815970
Jelle <jellewemmenhove@live.nl>
2024-08-23 16:29:20 +0200

    feat: 'Help'-tactic provides advice on how to use forall and exists hypotheses.

---
+++ tests/tactics/Help.v
@@ -0,0 +1,64 @@
+(******************************************************************************)
+(*                  This file is part of Waterproof-lib.                      *)
+(*                                                                            *)
+(*   Waterproof-lib is free software: you can redistribute it and/or modify   *)
+(*    it under the terms of the GNU General Public License as published by    *)
+(*     the Free Software Foundation, either version 3 of the License, or      *)
+(*                    (at your option) any later version.                     *)
+(*                                                                            *)
+(*     Waterproof-lib is distributed in the hope that it will be useful,      *)
+(*      but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         *)
+(*               GNU General Public License for more details.                 *)
+(*                                                                            *)
+(*     You should have received a copy of the GNU General Public License      *)
+(*   along with Waterproof-lib. If not, see <https://www.gnu.org/licenses/>.  *)
+(*                                                                            *)
+(******************************************************************************)
+
+Require Import Ltac2.Ltac2.
+Require Import Ltac2.Option.
+
+Require Import Waterproof.Tactics.Help.
+
+(** Test 0 : Suggest to follow advice in goal if goal is wrapped 
+  or [False] (i.e. prove contradiction). *)
+Goal False.
+  Help.
+Abort.
+
+(** Test 1 : Only suggest to follow advice in goal if goal is wrapped 
+  or [False] (i.e. prove contradiction). *)
+Goal (forall n : nat, n = n) -> False.
+  intros.
+  Help.
+Abort.
+
+(** Test 2 : Suggest to solve directly if goal can be shown automatically. *)
+Goal True.
+  Help.
+Abort.
+
+(** Test 3 : Only suggest to solve directly if goal can be shown automatically. *)
+Goal (forall n : nat, n = n) -> True.
+  intros.
+  Help.
+Abort.
+
+(** Test 4 : Report \forall hypotheses if available. *)
+Goal (forall n : nat, n = n) -> (forall m : nat, m + 1 = m + 1) -> (0 = 1).
+  intros.
+  Help.
+Abort.
+
+(** Test 5 : Report \exists hypotheses if available. *)
+Goal (exists n : nat, n = n) -> (exists m : nat, m + 1 = m + 1) -> (0 = 1).
+  intros.
+  Help.
+Abort.
+
+(** Test 6 : Report \forall and \exists hypotheses if available. *)
+Goal (forall n : nat, n = n) -> (exists m : nat, m = 0) -> (0 = 1).
+  intros.
+  Help.
+Abort.
