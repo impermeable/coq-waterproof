253ee7a9b5576f7e823cb014ec2f4978e1ab9155
Jim Portegies <j.w.portegies@tue.nl>
2024-10-13 12:16:26 +0200

    Partially convert the subsequences file

--- theories/Libs/Analysis/Subsequences.v
+++ theories/Libs/Analysis/Subsequences.v
@@ -25,12 +25,17 @@ Require Import ClassicalChoice.
 
 Require Import Automation.
 Require Import Libs.Negation.
-Require Import Notations.
+Require Import Notations.Common.
+Require Import Notations.Reals.
+Require Import Notations.Sets.
+Require Import Chains.
 Require Import Tactics.
 
 Require Import Waterprove.
 
 Waterproof Enable Automation RealsAndIntegers.
+
+(* TODO: this file is not fully converted to the new notations, but the definitions are *)
 
 (** ## Creating a subsequence of elements satisfying a certain property
 
@@ -38,14 +43,11 @@ The purpose of this section is to provide a somewhat general strategy to constru
 (** ### From existence of a next element to a function producing this element
 
 The next lemma is quite technical, and is usually not so visible in classical analysis. We rely here on a version of the axiom of choice.*)
-Lemma existence_next_el_to_fun :
-    ∀ (a : ℕ → ℝ) (P : ℕ → ℝ → Prop),
-    (∀ (m : ℕ) (N : ℕ), ∃ k : ℕ, (N ≤ k)%nat ∧ (P m (a k))) ⇒
-      ∃ f : ℕ → ℕ → ℕ, ∀ (m : ℕ) (N : ℕ), (N ≤ f m N)%nat ∧ P m (a (f m N)).
+Lemma existence_next_el_to_fun (a : ℕ → ℝ) (P : ℕ → ℝ → Prop) :
+    (∀ (m : ℕ), ∀ (N : ℕ), ∃ k : ℕ, (N ≤ k)%nat ∧ (P m (a k))) ⇒
+      ∃ f : ℕ → ℕ → ℕ, ∀ (m : ℕ), ∀ (N : ℕ), (N ≤ f m N)%nat ∧ P m (a (f m N)).
 Proof.
-    Take a : (ℕ → ℝ).
-    Take P : (ℕ → ℝ → Prop).
-    Assume that (for all m N : ℕ, there exists k : ℕ , (N ≤ k)%nat ∧ P m (a k)) (i).
+    Assume that (∀ m : ℕ, ∀ N : ℕ, there exists k : ℕ , (N ≤ k)%nat ∧ P m (a k)) (i).
     We claim that (∀ (m : ℕ),  ∃ g : ℕ → ℕ, ∀ N : ℕ, (N ≤ g N)%nat ∧ (P m (a (g N)))) (ii).
     {
         Take m : ℕ.
@@ -56,10 +58,9 @@ Proof.
     apply (ii).
 Qed.
 
-
 (** The next definition captures what it means to be an index sequence.*)
 Definition is_index_seq (n : ℕ → ℕ) :=
-    ∀ k : ℕ, (n k < n (S k))%nat.
+    ∀ k ∈ ℕ, (n k < n (S k))%nat.
 (** Given the function that produces 'good' elements, we can use it to construct a subsequence by induction.*)
 Fixpoint create_seq
   (f : ℕ → ℕ → ℕ) (l : ℕ) :=
@@ -68,34 +69,25 @@ Fixpoint create_seq
   | S k => f l (S (create_seq f k))
   end.
 
-
-
 (** If the function that produces 'good' elements is such that the produced elements are far enough in the sequence, it is certain that the produced sequence is an index sequence.*)
-Lemma created_seq_is_index_seq :
-  ∀ (g : ℕ → ℕ → ℕ),
-    (∀ (m N : ℕ), (g m N ≥ N)%nat ) ⇒
+Lemma created_seq_is_index_seq (g : ℕ → ℕ → ℕ) :
+    (∀ m : ℕ, ∀ N : ℕ, (g m N ≥ N)%nat ) ⇒
       is_index_seq (create_seq g).
 Proof.
-    Take g : (ℕ → ℕ → ℕ).
-    Assume that (∀ (m N : ℕ), (g m N ≥ N)%nat) (i).
-    We need to show that (for all k : ℕ, (create_seq g k < create_seq g (S k))%nat).
-    Take k : ℕ.
+    Assume that (∀ m : ℕ, ∀ N : ℕ, (g m N ≥ N)%nat) (i).
+    We need to show that (∀ k ∈ ℕ, (create_seq g k < create_seq g (S k))%nat).
+    Take k ∈ ℕ.
     By (i) it holds that (g (S k) (S (create_seq g k)) ≥ S(create_seq g k))%nat.
     We conclude that (& create_seq g k < g (S k) (S (create_seq g k)) = create_seq g (S k))%nat.
 Qed.
 
-
 (**
 The next lemma records that indeed the elements in the subsequence satisfy the desired property.*)
-Lemma subseq_sat_rel :
-  ∀ (a : ℕ → ℝ) (g : ℕ → ℕ → ℕ) (P : ℕ → ℝ → Prop),
-    (∀ m N : ℕ, P m (a (g m N)) ) ⇒
+Lemma subseq_sat_rel (a : ℕ → ℝ) (g : ℕ → ℕ → ℕ) (P : ℕ → ℝ → Prop) :
+    (∀ m : ℕ, ∀ N : ℕ, P m (a (g m N)) ) ⇒
       ∀ k : ℕ, P k (a (create_seq g k)).
 Proof.
-    Take a : (ℕ → ℝ).
-    Take g : (ℕ → ℕ → ℕ).
-    Take P : (ℕ → ℝ → Prop).
-    Assume that (∀ m N : ℕ, P m (a (g m N))) (i).
+    Assume that (∀ m : ℕ, ∀ N : ℕ, P m (a (g m N))) (i).
     induction k. (*TODO: if we use 'We use induction on k.'-tacftic, then we cannot directly match on k + 1 *)
     - We need to show that (P 0%nat (a (g 0%nat 0%nat))).
       By (i) we conclude that (P 0%nat (a (g 0%nat 0%nat))).
@@ -103,33 +95,28 @@ Proof.
       By (i) we conclude that (P (S k) (a (g (S k) (S (create_seq g k))))).
 Qed.
 
-
-
-Lemma exists_good_subseq :
-  ∀ (a : ℕ → ℝ) (P : ℕ → ℝ → Prop),
-    (∀ (m : ℕ) (N₀ : ℕ), ∃ k : ℕ, (N₀ ≤ k)%nat ∧ (P m (a k))) ⇒
+Lemma exists_good_subseq (a : ℕ → ℝ) (P : ℕ → ℝ → Prop) :
+    (∀ (m : ℕ), ∀ (N₀ : ℕ), ∃ k : ℕ, (N₀ ≤ k)%nat ∧ (P m (a k))) ⇒
       ∃ n : ℕ → ℕ, is_index_seq n ∧ ∀ k : ℕ, P k (a (n k)).
 Proof.
-    Take a : (ℕ → ℝ).
-    Take P : (ℕ → ℝ → Prop).
-    Assume that (for all m N : ℕ, there exists k : ℕ , (N ≤ k)%nat ∧ P m (a k)).
+    Assume that (∀ m : ℕ, ∀ N : ℕ, there exists k : ℕ , (N ≤ k)%nat ∧ P m (a k)).
     By existence_next_el_to_fun it holds that
-      (∃ g : ℕ → ℕ → ℕ, ∀ (m : ℕ) (N : ℕ), (N ≤ g m N)%nat ∧ P m (a (g m N))).
+      (∃ g : ℕ → ℕ → ℕ, ∀ (m : ℕ), ∀ (N : ℕ), (N ≤ g m N)%nat ∧ P m (a (g m N))).
     Obtain such a g. It holds that
-      (∀ (m : ℕ) (N : ℕ), (N ≤ g m N)%nat ∧ P m (a (g m N))) (i).
+      (∀ (m : ℕ), ∀ (N : ℕ), (N ≤ g m N)%nat ∧ P m (a (g m N))) (i).
     Choose n := (create_seq g).
     We show both statements.
     - We need to show that (is_index_seq n).
       We need to show that (is_index_seq (create_seq g)).
       By created_seq_is_index_seq it suffices to show that
-        (for all m M : ℕ, (g m M ≥ M)%nat).
+        (∀ m : ℕ, ∀ M : ℕ, (g m M ≥ M)%nat).
       Take m, M : nat.
       By (i) it holds that ((M ≤ g m M)%nat ∧ P m (a (g m M))).
       We conclude that (g m M >= M)%nat.
     - We need to show that (for all k : ℕ, P k (a (n k))).
       We need to show that (for all k : ℕ, P k (a ((create_seq g) k))).
       Fail By subseq_sat_rel it suffices to show that
-        (for all m M : ℕ, P m (a (g m M))). (*TODO: fix*)
+        (∀ m : ℕ, ∀ M : ℕ, P m (a (g m M))). (*TODO: fix*)
       apply subseq_sat_rel.
       Take m : ℕ.
       We need to show that (for all M : ℕ, P(m, a (g(m, M)))).
@@ -140,16 +127,15 @@ Proof.
 Qed.
 
 Definition is_increasing (f : ℕ → ℕ) :=
-  ∀ k : ℕ, (f k ≤ f (S k))%nat.
-Lemma incr_loc_to_glob :
-  ∀ g : ℕ → ℕ,
+  ∀ k ∈ ℕ, (f k ≤ f (S k))%nat.
+
+Lemma incr_loc_to_glob (g : ℕ → ℕ) :
     is_increasing g
-      ⇒ (∀ k l : ℕ, (k ≤ l)%nat ⇒ (g k ≤ g l)%nat).
+      ⇒ (∀ k : ℕ, ∀ l : ℕ, (k ≤ l)%nat ⇒ (g k ≤ g l)%nat).
 Proof.
-    Take g : (ℕ → ℕ).
-    We need to show that ((for all k : ℕ, (g k ≤ g (S k))%nat)
-      ⇨ for all k l : ℕ, (k ≤ l)%nat ⇨ (g k ≤ g l)%nat).
-    Assume that (∀ k : ℕ, (g k ≤ g (S k))%nat).
+    We need to show that ((∀ k ∈ ℕ, (g k ≤ g (S k))%nat)
+      ⇨ ∀ k : ℕ, ∀ l : ℕ, (k ≤ l)%nat ⇨ (g k ≤ g l)%nat).
+    Assume that (∀ k ∈ ℕ, (g k ≤ g (S k))%nat).
     Take k : ℕ.
     induction l as [|l IH_l].
     (** We first need to show that if $k \leq 0$ then $(f (k) \leq f(0))$.*)
@@ -173,24 +159,21 @@ Proof.
     Contradiction.
 Qed.
 
-
-Lemma index_seq_strictly_incr :
-  ∀ n : ℕ → ℕ, is_index_seq n ⇒ (is_increasing (fun (k : ℕ) ↦ (n k - k)%nat)).
+Lemma index_seq_strictly_incr (n : ℕ → ℕ) :
+  is_index_seq n ⇒ (is_increasing (fun (k : ℕ) ↦ (n k - k)%nat)).
 Proof.
-    Take n : (ℕ → ℕ); such that (is_index_seq n).
-    We need to show that (for all k : ℕ, (n(k) - k ≤ n(S(k)) - S(k))%nat).
-    Take k : ℕ.
+    Assume that (is_index_seq n).
+    We need to show that (∀ k ∈ ℕ, (n(k) - k ≤ n(S(k)) - S(k))%nat).
+    Take k ∈ ℕ.
     It holds that (for all k0 : ℕ, (n k0 < n (S k0))%nat).
     It holds that (n k < n (S k))%nat.
     We conclude that (n k - k ≤ n (S k) - S k)%nat.
 Qed.
 
-
-
-Lemma index_seq_grows_0 :
-  ∀ n : ℕ → ℕ, is_index_seq n ⇒ ∀ k : ℕ, (n k ≥ k)%nat.
+Lemma index_seq_grows_0 (n : ℕ → ℕ) :
+  is_index_seq n ⇒ ∀ k : ℕ, (n k ≥ k)%nat.
 Proof.
-    Take n : (ℕ → ℕ); such that (is_index_seq n).
+    Assume that (is_index_seq n).
     induction k as [|k IH].
     - We conclude that (n 0 >= 0)%nat.
     - It holds that (for all k0 : ℕ, (n k0 < n (S k0))%nat).
@@ -198,20 +181,17 @@ Proof.
       We conclude that (n (S k) ≥ S k)%nat.
 Qed.
 
-
-Lemma index_seq_grows :
-  ∀ n : ℕ → ℕ, is_index_seq n ⇒ (∀ k l : ℕ, (k ≤ l)%nat ⇒ (n k - k ≤ n l - l)%nat).
+Lemma index_seq_grows (n : ℕ → ℕ) :
+  is_index_seq n ⇒ (∀ k : ℕ, ∀ l : ℕ, (k ≤ l)%nat ⇒ (n k - k ≤ n l - l)%nat).
 Proof.
-    Take n : (ℕ → ℕ); such that (is_index_seq n).
+    Assume that (is_index_seq n).
     Define g := (fun (k : ℕ) ↦ (n k - k)%nat).
     By index_seq_strictly_incr it holds that (is_increasing g).
-    By incr_loc_to_glob it holds that (∀ k l : ℕ, (k ≤ l)%nat ⇒ (g k ≤ g l)%nat).
+    By incr_loc_to_glob it holds that (∀ k : ℕ, ∀ l : ℕ, (k ≤ l)%nat ⇒ (g k ≤ g l)%nat).
     Take k : ℕ and l : ℕ; such that(k ≤ l)%nat.
     We need to show that (g k <= g l)%nat.
     We conclude that (g k <= g l)%nat.
 Qed.
-
-
 
 (** ## Constructing a subsequence with elements satisfying a particular property
 
@@ -235,16 +215,12 @@ Definition first_satisfying_element
   (N : ℕ)
   := first_satisfying_element_helper rel (N-l) N.
 (** ### From infinitely many elements to a function producing those elements*)
-Lemma inf_el_to_fun :
-  ∀ (a : ℕ → ℝ) (P : ℕ → ℝ → Prop),
+Lemma inf_el_to_fun (a : ℕ → ℝ) (P : ℕ → ℝ → Prop) :
     (∀ N : ℕ, ∃ k : ℕ, (N ≤ k)%nat ∧ (P N (a k))) ⇒
       ∃ f : ℕ → ℕ, ∀ l : ℕ, (l ≤ f l)%nat ∧ P l (a (f l)).
 Proof.
-    Take a : (ℕ → ℝ).
-    Take P : (ℕ → ℝ → Prop).
     apply choice with (R := fun (k : ℕ) (l : ℕ) ↦ ((k ≤ l)%nat ∧ P k (a l))).
 Qed.
-
 
 Fixpoint seq_of_max (f : ℕ → ℕ) (l : ℕ) :=
   match l with
@@ -254,23 +230,20 @@ Fixpoint seq_of_max (f : ℕ → ℕ) (l : ℕ) :=
 
 
 (** ### The sequence of maxima is increasing*)
-Lemma seq_of_max_is_increasing :
-  ∀ g : ℕ → ℕ, is_increasing (seq_of_max g).
+Lemma seq_of_max_is_increasing (g : ℕ → ℕ) :
+  is_increasing (seq_of_max g).
 Proof.
-    Take g : (ℕ → ℕ).
     We need to show that
-      (for all k : ℕ, (seq_of_max g k ≤ seq_of_max g (S k))%nat).
-    Take k : ℕ.
+      (∀ k ∈ ℕ, (seq_of_max g k ≤ seq_of_max g (S k))%nat).
+    Take k ∈ ℕ.
     We need to show that (seq_of_max g k ≤ Nat.max (g (S k)) (seq_of_max g k))%nat.
     We conclude that (seq_of_max g k ≤ Nat.max (g (S k)) (seq_of_max g k))%nat.
 Qed.
 
 
-Lemma elements_le_seq_of_max_pre :
-  ∀ (g : ℕ → ℕ) (n : ℕ),
+Lemma elements_le_seq_of_max_pre (g : ℕ → ℕ) (n : ℕ) :
     (g n ≤ seq_of_max g n)%nat.
 Proof.
-    Take g : (ℕ → ℕ).
     We use induction on n.
     - We first show the base case (g(0) ≤ seq_of_max(g, 0))%nat.
       We need to show that (g 0 ≤ g 0)%nat.
@@ -288,13 +261,10 @@ Proof.
       We conclude that (g(n) ≤ seq_of_max(g, n))%nat.
 Qed.
 
-
-Lemma elements_le_seq_of_max :
-  ∀ (g : ℕ → ℕ) (n : ℕ) (k : ℕ),
+Lemma elements_le_seq_of_max (g : ℕ → ℕ) (n : ℕ) (k : ℕ) :
     (k ≤ n)%nat ⇒ (g k ≤ seq_of_max g n)%nat.
 Proof.
-    Take g : (ℕ → ℕ).
-    Take n : ℕ and k : ℕ; such that (k ≤ n)%nat.
+    Assume that (k ≤ n)%nat.
     By elements_le_seq_of_max_pre it holds that (g k ≤ seq_of_max g k)%nat.
     We claim that (seq_of_max g k ≤ seq_of_max g n)%nat.
     { By incr_loc_to_glob it suffices to show that (is_increasing (seq_of_max g)).
@@ -312,15 +282,13 @@ Fixpoint build_seq
   | O => f O
   | S m => f (S (seq_of_max f (build_seq f m)))
   end.
-Lemma built_seq_is_index_seq :
-  ∀ g : ℕ → ℕ,
+Lemma built_seq_is_index_seq (g : ℕ → ℕ) :
     (∀ k : ℕ, (g k ≥ k)%nat) ⇒
       is_index_seq (build_seq g).
 Proof.
-    Take g : (ℕ → ℕ).
     Assume that (for all k : ℕ, (g k ≥ k)%nat).
-    We need to show that (for all k : ℕ, (build_seq g k < build_seq g (S k))%nat).
-    Take k : ℕ.
+    We need to show that (∀ k ∈ ℕ, (build_seq g k < build_seq g (S k))%nat).
+    Take k ∈ ℕ.
     We need to show that (build_seq g k < g (S (seq_of_max g (build_seq g k))))%nat.
     It holds that (g( S(seq_of_max g (build_seq g k)))≥ S(seq_of_max g (build_seq g k)))%nat.
     It holds that (g( S(seq_of_max g (build_seq g k)))> seq_of_max g (build_seq g k))%nat.
