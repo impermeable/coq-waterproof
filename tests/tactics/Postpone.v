02669523695ea3b7ab8d3ca368e41f8e4ecfc746
Jim Portegies <j.w.portegies@tue.nl>
2024-10-20 16:59:33 +0200

    Fix that goals can be closed after choosing blanks or using magic

--- tests/tactics/Postpone.v
+++ tests/tactics/Postpone.v
@@ -59,3 +59,8 @@ Proof.
   assert_feedback_with_string (fun () => By magic we conclude that (0 = 1)) Warning
 "Please come back later to provide an actual proof of (0 = 1).".
 Abort.
+
+Goal False.
+  assert_feedback_with_string (fun () => By magic it suffices to show that (True)) Warning
+"Please come back later to prove that it suffices to show that True".
+Abort.
