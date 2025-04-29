c8968f0d87a414c58950689c0dfb03aee95ed38f
Pim Otte <p.j.otte@uu.nl>
2025-04-29 08:59:47 +0200

    Port SequentialAccumulationPoints

--- theories/Libs/Analysis/SequentialAccumulationPoints.v
+++ theories/Libs/Analysis/SequentialAccumulationPoints.v
@@ -41,39 +41,39 @@ Definition is_seq_acc_pt (a : ℕ → ℝ) (x : ℝ) : Prop :=
 Lemma seq_bdd_seq_acc_pts_bdd (a : ℕ → ℝ) :
   has_ub a ⇒ bound (is_seq_acc_pt a).
 Proof.
-  Assume that (has_ub a) as (i).
+  Assume that has_ub a as (i).
   We need to show that
-    (there exists m : ℝ, is_upper_bound (is_seq_acc_pt a) m).
-  We need to show that (there exists m : ℝ,
-    for all x : ℝ, is_seq_acc_pt a x ⇨ x ≤ m).
-  By (i) it holds that (there exists M : R, is_upper_bound (EUn a) M).
-  Obtain such an M. It holds that (is_upper_bound (EUn a) M).
+    there exists m : ℝ, is_upper_bound (is_seq_acc_pt a) m.
+  We need to show that there exists m : ℝ,
+    for all x : ℝ, is_seq_acc_pt a x ⇨ x ≤ m.
+  By (i) it holds that there exists M : R, is_upper_bound (EUn a) M.
+  Obtain such an M. It holds that is_upper_bound (EUn a) M.
   Choose m := M.
   Take x : ℝ.
-  Assume that (is_seq_acc_pt a x).
-  It holds that (∃ n : ℕ ⇨ ℕ,
-    is_index_seq n ∧ converges_to (fun k ↦ a(n(k)), x)).
+  Assume that is_seq_acc_pt a x.
+  It holds that ∃ n : ℕ ⇨ ℕ,
+    is_index_seq n ∧ converges_to (fun k ↦ a(n(k)), x).
   Obtain such an n.
-  It holds that (is_index_seq n ∧ converges_to (fun k ↦ a(n(k)), x)) as (iv).
-  Because (iv) both (is_index_seq n) and (converges_to (fun k ↦ (a(n(k))), x)) hold.
-  We need to show that (x ≤ M).
+  It holds that is_index_seq n ∧ converges_to (fun k ↦ a(n(k)), x) as (iv).
+  Because (iv) both is_index_seq n and converges_to (fun k ↦ (a(n(k))), x) hold.
+  We need to show that x ≤ M.
   (*FIXME*)
   (* By (upp_bd_seq_is_upp_bd_lim (fun k => a (n k))) it suffices to show that
     (∀ k ∈ nat, (a (n k) <= M)).*)
   enough (∀ k ∈ nat, (a (n k) <= M)) by (
   apply (upp_bd_seq_is_upp_bd_lim (fun k ↦ a (n k))); assumption).
-  We claim that (for all k : ℕ, (a k) ≤ M) as (v).
-  { It holds that (for all x0 : ℝ, EUn a x0 ⇨ x0 ≤ M).
-    It holds that (for all x0 : ℝ,
-      (there exists k : ℕ, x0 = a k) ⇨ x0 ≤ M).
+  We claim that for all k : ℕ, (a k) ≤ M as (v).
+  { It holds that for all x0 : ℝ, EUn a x0 ⇨ x0 ≤ M.
+    It holds that for all x0 : ℝ,
+      (there exists k : ℕ, x0 = a k) ⇨ x0 ≤ M.
     Take k : ℕ.
-    It suffices to show that (there exists k0 : nat, a k = a k0).
+    It suffices to show that there exists k0 : nat, a k = a k0.
     Choose k0 := k.
-    We conclude that (a k = a k0).
+    We conclude that a k = a k0.
   }
   Take k ∈ ℕ.
-  By (v) it holds that (a(n k) ≤ M).
-  It follows that (a(n k) ≤ M).
+  By (v) it holds that a(n k) ≤ M.
+  It follows that a(n k) ≤ M.
 Qed.
 
 Close Scope R_scope.
