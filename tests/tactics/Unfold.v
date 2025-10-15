c0339854bd3080418bcfe6a0fbee8293a63c4623
Jim Portegies <j.w.portegies@tue.nl>
2025-10-15 14:43:29 +0200

    Add tests for infimum and supremum

--- tests/tactics/Unfold.v
+++ tests/tactics/Unfold.v
@@ -122,19 +122,75 @@ It suffices to show that P.
 It holds that P.
 Abort.
 
+From Stdlib Require Import Reals.Reals.
+Require Import Waterproof.Notations.Common.
+Require Import Waterproof.Notations.Reals.
+Require Import Waterproof.Notations.Sets.
+Require Import Waterproof.Libs.Analysis.SupAndInf.
+
+
+Open Scope R_scope.
+
+Local Parameter A : subset R.
+
+(* Test 9, test for infimum, as it is important that it works in practice. *)
+Goal 4 is the infimum of A -> 3 is the infimum of A.
+Proof.
+intro H.
+assert_feedback_with_strings
+  (fun () =>
+  assert_fails_with_string
+  (fun () => Expand the definition of infimum)
+"Remove this line in the final version of your proof.")
+  Info
+["Expanded definition in statements where applicable.";
+"Hint, insert: We need to show that (3 is a _lower bound_ for A
+                      ∧ (∀ l ∈ ℝ, l is a _lower bound_ for A ⇨ l ≤ 3)).";
+"Hint, insert: It holds that (4 is a _lower bound_ for A
+               ∧ (∀ l ∈ ℝ, l is a _lower bound_ for A ⇨ l ≤ 4)).";
+"Applied alternative characterizations in statements where applicable.";
+"Hint, insert: It suffices to show that (3 is a _lower bound_ for A
+                          ∧ (∀ ε > 0, ∃ a ∈ A, a < 3 + ε)).";
+"Hint, insert: It holds that (4 is a _lower bound_ for A ∧ (∀ ε > 0, ∃ a ∈ A, a < 4 + ε))."].
+Abort.
+
+(* Test 10, test for supremum, as it is important that it works in practice. *)
+Goal 4 is the supremum of A -> 3 is the supremum of A.
+Proof.
+intro H.
+assert_feedback_with_strings
+  (fun () =>
+  assert_fails_with_string
+  (fun () => Expand the definition of supremum)
+"Remove this line in the final version of your proof.")
+  Info
+["Expanded definition in statements where applicable.";
+"Hint, insert: We need to show that (3 is an _upper bound_ for A
+                      ∧ (∀ L ∈ ℝ, L is an _upper bound_ for A ⇨ 3 ≤ L)).";
+"Hint, insert: It holds that (4 is an _upper bound_ for A
+               ∧ (∀ L ∈ ℝ, L is an _upper bound_ for A ⇨ 4 ≤ L)).";
+"Applied alternative characterizations in statements where applicable.";
+"Hint, insert: It suffices to show that (3 is an _upper bound_ for A
+                          ∧ (∀ ε > 0, ∃ a ∈ A, 3 - ε < a)).";
+"Hint, insert: It holds that (4 is an _upper bound_ for A ∧ (∀ ε > 0, ∃ a ∈ A, 4 - ε < a))."].
+Abort.
+
+Close Scope R_scope.
+Open Scope nat_scope.
+
 (** Check unfolding method that does not throw an error.
   Meant for internal use by custom Waterproof editor. *)
 
 (** Non-framework version. *)
 
-(* Test 9: unfold term in hypotheses and goal without throwing an error. *)
+(* Test 11: unfold term in hypotheses and goal without throwing an error. *)
 Goal (foo = 0) -> (foo = 2) -> (foo = 1).
 Proof.
   intros.
   _internal_ Expand the definition of foo.
 Abort.
 
-(* Test 10: unfold fails to unfold term if no statement with term. *)
+(* Test 12: unfold fails to unfold term if no statement with term. *)
 Goal False.
 Proof.
   _internal_ Expand the definition of foo.
@@ -145,14 +201,14 @@ Abort.
 Ltac2 Notation "_internal_" "Expand" "the" "definition" "of" "foo2" :=
   wp_unfold unfold_foo (Some "foo2") false true None.
 
-(* Test 11: unfold term in hypotheses and goals. *)
+(* Test 13: unfold term in hypotheses and goals. *)
 Goal (foo = 0) -> (foo = 2) -> (foo = 1).
 Proof.
   intros.
   _internal_ Expand the definition of foo2.
 Abort.
 
-(* Test 12: fails to unfold term if no statements with term. *)
+(* Test 13: fails to unfold term if no statements with term. *)
 Goal False.
 Proof.
    _internal_ Expand the definition of foo2.
